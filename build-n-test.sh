5eadccd6ff2c6ade2da877819c160c91ff6cfac1
Jonathan Springer <jonpspri@gmail.com>
2020-01-07 12:38:37 -0500

    Added crow and optimized

--- build-n-test.sh
+++ build-n-test.sh
@@ -3,17 +3,17 @@
 set -euo pipefail
 scriptdir="$(dirname "$(realpath "$0")")"
 
-for i in cxx-restinio go java-quarkus; do (
+for i in cxx-crow cxx-restinio go java-quarkus; do (
     echo "-------------------------------------------------------------------"
     echo "Building $i..."
     # TODO Use parallel to speed this part up?
-    docker build -t "multiarch-demo-$i" "$scriptdir/$i" >"$scriptdir/log/$i-build.out"
+    docker build -t "distroless-demo-$i" "$scriptdir/$i" >"$scriptdir/log/$i-build.out"
     echo "Running $i..."
     docker rm "$i" 2> /dev/null || :
-    docker run --init --name="$i" -d -p 8080:8080 "multiarch-demo-$i:latest"
+    docker run --init --name="$i" -d -p 8080:8080 "distroless-demo-$i:latest"
     sleep 5  # TODO -- change this into a loop with faster checks?
-    curl -s http://localhost:8080/api/hello || :
-    echo
+    curl -si http://localhost:8080/api/hello || :; echo
+    curl -si http://localhost:8080/bogus || :; echo
     docker rm -f "$i"
     echo "-------------------------------------------------------------------"
 )| tee "$scriptdir/log/$i.out"; done
--- cxx-restinio/src/helloworld.cpp
+++ cxx-restinio/src/helloworld.cpp
@@ -1,13 +1,56 @@
 #include <restinio/all.hpp>
+
+template < typename RESP >
+RESP
+init_resp( RESP resp )
+{
+    resp.append_header( restinio::http_field::server, "RESTinio sample server /v.0.2" );
+    resp.append_header_date_field();
+    return resp;
+}
+
+using router_t = restinio::router::express_router_t<>;
+auto create_request_handler()
+{
+    auto router = std::make_unique< router_t >();
+    router->http_get(
+        "/api/hello",
+        []( auto req, auto ){
+                init_resp( req->create_response() )
+                    .append_header( restinio::http_field::content_type, "text/plain; charset=utf-8" )
+                    .set_body( "Hello, world!")
+                    .done();
+                return restinio::request_accepted();
+        } );
+
+    router->non_matched_request_handler(
+        []( auto req ){
+            return req->create_response(restinio::status_not_found())
+                .connection_close()
+                .done();
+        } );
+    return router;
+}
+
 int main()
 {
-    restinio::run(
-        restinio::on_this_thread()
-        .port(8080)
-        //.address("localhost")
-        .request_handler([](auto req) {
-            // TODO - use express routers to confine this to one path
-            return req->create_response().set_body("Hello, World!").done();
-        }));
+    using namespace std::chrono;
+    try
+    {
+        using traits_t =
+            restinio::traits_t<
+                restinio::asio_timer_manager_t,
+                restinio::single_threaded_ostream_logger_t,
+                router_t >;
+        restinio::run(
+            restinio::on_this_thread<traits_t>()
+                .port( 8080 )
+                .request_handler( create_request_handler() ) );
+    }
+    catch( const std::exception & ex )
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
